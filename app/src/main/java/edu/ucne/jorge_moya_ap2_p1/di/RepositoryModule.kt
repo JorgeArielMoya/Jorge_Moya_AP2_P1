1461a9ca5cc35c9552a3de9f40f7ea93ccd248ff
jorge <jorge@LAPTOP-D8GDLQRL>
2026-06-10 19:56:20 -0400

    Provee el repositorio de amonestacion

--- app/src/main/java/edu/ucne/jorge_moya_ap2_p1/di/RepositoryModule.kt
+++ app/src/main/java/edu/ucne/jorge_moya_ap2_p1/di/RepositoryModule.kt
@@ -1,4 +1,20 @@
 package edu.ucne.jorge_moya_ap2_p1.di
 
-class RepositoryModule {
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.jorge_moya_ap2_p1.data.repository.AmonestacionRepositoryImpl
+import edu.ucne.jorge_moya_ap2_p1.domain.repository.AmonestacionRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindsRepositoryModule (
+        impl: AmonestacionRepositoryImpl
+    ) : AmonestacionRepository
 }
