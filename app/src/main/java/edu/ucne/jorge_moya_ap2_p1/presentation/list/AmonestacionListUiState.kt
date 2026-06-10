e1c95a00b44ba37b1cac2efc639cd1c9f26c8b18
jorge <jorge@LAPTOP-D8GDLQRL>
2026-06-10 19:59:32 -0400

    Trabaja los estados de la lista

---
+++ app/src/main/java/edu/ucne/jorge_moya_ap2_p1/presentation/list/AmonestacionListUiState.kt
@@ -0,0 +1,14 @@
+package edu.ucne.jorge_moya_ap2_p1.presentation.list
+
+import edu.ucne.jorge_moya_ap2_p1.domain.model.Amonestacion
+
+data class AmonestacionListUiState (
+    val isLoading : Boolean = false,
+    val searchNombre: String = "",
+    val amonestaciones : List<Amonestacion> = emptyList(),
+    val amonestacionesFiltradas: List<Amonestacion> = emptyList(),
+    val message : String? = null,
+    val error : String? = null,
+    val onNavigateToCreate : Boolean = false,
+    val onNavigateToEdit : Int? = null
+)
