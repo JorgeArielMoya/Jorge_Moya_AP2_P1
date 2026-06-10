e1a1a48bba5d5d172e1da667730caa729490a1a8
jorge <jorge@LAPTOP-D8GDLQRL>
2026-06-10 19:56:01 -0400

    Trabaja el modulo de la app para agregar proveer la base de datos y el dao

--- app/src/main/java/edu/ucne/jorge_moya_ap2_p1/di/AppModule.kt
+++ app/src/main/java/edu/ucne/jorge_moya_ap2_p1/di/AppModule.kt
@@ -1,4 +1,33 @@
 package edu.ucne.jorge_moya_ap2_p1.di
 
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.jorge_moya_ap2_p1.data.local.dao.AmonestacionDao
+import edu.ucne.jorge_moya_ap2_p1.data.local.db.Database
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesDatabase (@ApplicationContext context: Context) : Database{
+        return Room.databaseBuilder(
+            context,
+            Database::class.java,
+            "database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesAmonestacionDao (database: Database) : AmonestacionDao{
+        return database.amonestacionDao()
+    }
 }
