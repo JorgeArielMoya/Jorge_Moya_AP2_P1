1de0dacf239907f665382361920e9b0a6a5b5892
jorge <jorge@LAPTOP-D8GDLQRL>
2026-06-10 19:54:47 -0400

    Trabaja el modelo de amonestacion

---
+++ app/src/main/java/edu/ucne/jorge_moya_ap2_p1/domain/model/Amonestacion.kt
@@ -0,0 +1,8 @@
+package edu.ucne.jorge_moya_ap2_p1.domain.model
+
+data class Amonestacion (
+    val amonestacionId :Int,
+    val nombres : String,
+    val razon : String,
+    val monto : Double
+)
